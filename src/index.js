e04e83a0f73fccd06192fbfea1ac44bff97110ae
Nilp Amin <nilpamin2@gmail.com>
2022-01-29 12:35:11 +1000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+import help from "./help.svg";
+import setting from "./setting.svg";
+
+class Board extends React.Component {
+    render() {
+        return (
+            <div className="todo">
+                <p>Board Implementation To Come Soon...</p>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <header>
+                    <div className="menu">
+                        <button id="help-button">
+                            <img src={help}></img>
+                        </button>
+                    </div>
+                    <div className="title">
+                        <h1>WORDLE</h1>
+                    </div>
+                    <div>
+                        <button id="setting-button">
+                            <img src={setting}></img>
+                        </button>
+                    </div>
+                </header>
+                <div id="board">
+                    <Board />
+                </div>
+            </div>
+            
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById("root"),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
