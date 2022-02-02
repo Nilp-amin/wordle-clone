eb2b32870d1b66a85a9abd349d28b3ae4c1c2450
Nilp Amin <nilpamin2@gmail.com>
2022-02-03 00:37:15 +1000

    Added color checking and word checking logic

--- src/index.js
+++ src/index.js
@@ -43,22 +43,53 @@ class BoardRow extends React.Component {
 }
 
 class Board extends React.Component {
+    tileEmpty = "#000000";
+    tileColorAbsent = "#3a3a3c";
+    tileColorPresent = "#b59f3b";
+    tileColorCorrect = "#538d4e";
     constructor(props) {
         super(props);
-        const tileColorAbsent = "#3a3a3c";
-        const tileColorPresent = "#b59f3b";
-        const tileColorCorrect = "#538d4e";
         this.state = {
             boardState: Array.from({length: 6}, e => Array(5).fill(null)),
-            boardColor: Array.from({length: 6}, e => Array(5).fill(tileColorAbsent)),
+            boardColor: Array.from({length: 6}, e => Array(5).fill(this.tileEmpty)),
             activeRow: 0,
             activeTileIndex: 0,
+            correctWord: "hello",
         }
 
         this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
-    // TODO: Check if row char after 5 letters entered and "enter" key pressed
+    checkUserGuess(guess) {
+        const correctWord = this.state.correctWord;
+
+        // Check if guess is correct
+        if (guess === this.state.correctWord) {
+            return Array(5).fill(this.tileColorCorrect);
+        }
+
+        const correctWordCharCount = {};
+        for (let i = 0; i < correctWord.length; i++) {
+            correctWordCharCount[correctWord.charAt(i)] = 
+                correctWord.split(correctWord.charAt(i)).length - 1;
+        }
+
+        // Check if any letters match correct word
+        const tileColors = Array(5).fill(this.tileColorAbsent);
+        for (let i = 0; i < correctWord.length; i++) {
+            if (correctWord.includes(guess.charAt(i)) && correctWordCharCount[guess.charAt(i)] > 0) {
+                if (correctWord.indexOf(guess.charAt(i)) === i) {
+                    tileColors[i] =  this.tileColorCorrect;
+                } else {
+                    tileColors[i] = this.tileColorPresent;
+                }
+                correctWordCharCount[guess.charAt(i)] = --correctWordCharCount[guess.charAt(i)];
+            }
+        }
+
+        return tileColors;
+
+    }
 
     handleKeyDown(e) {
         const keyPressed = e.key;
@@ -67,14 +98,10 @@ class Board extends React.Component {
         var tileIndex = this.state.activeTileIndex;
         if (keyPressed.length === 1 && keyPressed.match(/[a-z]/i)) {
            if (boardRow < boardState.length && tileIndex < boardState[0].length) {
-               boardState[boardRow][tileIndex] = keyPressed;
+               boardState[boardRow][tileIndex] = keyPressed.toLowerCase();
            }
            if (tileIndex < boardState[0].length) {
                tileIndex++;
-           }
-           if (boardRow < boardState.length && tileIndex === boardState[0].length) {
-               tileIndex = 0;
-               boardRow++;
            }
            this.setState(
                {
@@ -84,24 +111,35 @@ class Board extends React.Component {
                }
            );
         } else if (keyPressed === "Backspace") {
+            // TODO: Fix backspace key error after last row and last letter filled
             if (tileIndex > 0) {
                 tileIndex--;
-            } else if (boardRow > 0) {
-                tileIndex = 4;
-                boardRow--;
             }
             boardState[boardRow][tileIndex] = null;
 
-            this.setState(
-                {
+            this.setState({
                     boardState: boardState,
                     activeTileIndex: tileIndex,
                     activeRow: boardRow,
-                }
-            );
+            });
+        } else if (keyPressed === "Enter" && tileIndex === boardState[0].length) {
+            const boardColor = this.state.boardColor;
+            boardColor[boardRow] = this.checkUserGuess(this.state.boardState[boardRow].join(""));
+
+            tileIndex = 0;
+            if (boardRow < boardState.length) {
+                boardRow++;
+            }
+
+            this.setState({
+                boardColor: boardColor,
+                activeRow: boardRow,
+                activeTileIndex: tileIndex,
+            });
         }
     }
 
+    // TODO: Add keyboard to webpage
     render() {
         window.addEventListener("keydown", this.handleKeyDown);
         return (
